d2f19d1f3751f1048baa271eb7f6491f0742599c
td0034 <t.didiotcook@gmail.com>
2023-02-15 15:49:31 +0000

    blink working

---
+++ ESP32_dev/Blink_WROOM_32D/src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+
+int ledPin = 2;
+
+void setup() 
+{
+  // put your setup code here, to run once:
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+  digitalWrite(ledPin, HIGH);
+  delay(500);
+  digitalWrite(ledPin, LOW);
+  delay(500);  
+}
